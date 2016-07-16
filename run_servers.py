34d836fef83c053bdaba73da60cab4f12681be9c
jeeeberhardt <qksoneo@gmail.com>
2016-07-16 15:13:17 +0200

    add docstring

--- run_servers.py
+++ run_servers.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+""" Start Pymol and Bokeh server """
+
 from __future__ import print_function
 
 import time
--- visualize.py
+++ visualize.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+""" Visualize MD trajectories using Pymol and Bokeh """
+
 from __future__ import print_function
 
 import os
