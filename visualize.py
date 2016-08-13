0dd78902561d09b0b19e91fca0a8b1c4dbb2cb4d
jeeeberhardt <qksoneo@gmail.com>
2016-08-13 16:06:29 +0200

    add energy

--- visualize.py
+++ visualize.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 import os
 import random
 import argparse
+import warnings
 import subprocess
 import numpy as np
 
@@ -18,6 +19,8 @@ from bokeh.models import HoverTool, ColumnDataSource
 from bokeh.plotting import output_server, figure, curdoc
 from matplotlib.cm import get_cmap
 from matplotlib import colors
+
+warnings.filterwarnings("ignore")
 
 __author__ = "Jérôme Eberhardt, Roland H Stote, and Annick Dejaegere"
 __copyright__ = "Copyright 2016, Jérôme Eberhardt"
@@ -102,6 +105,29 @@ def get_comments_from_txt(fname, comments='#'):
 def generate_color(value, cmap):
     return colors.rgb2hex(get_cmap(cmap)(value))
 
+def assignbins2D(coordinates, bin_size):
+
+    x_min, x_max = np.min(coordinates[:,0]), np.max(coordinates[:,0])
+    y_min, y_max = np.min(coordinates[:,1]), np.max(coordinates[:,1])
+
+    x_length = (x_max - x_min)
+    y_length = (y_max - y_min)
+
+    x_center = x_min + (x_length/2)
+    y_center = y_min + (y_length/2)
+
+    if x_length > y_length:
+        x_limit = np.array([x_center-(x_length/2)-0.5, x_center+(x_length/2)+0.5])
+        y_limit = np.array([y_center-(x_length/2)-0.5, y_center+(x_length/2)+0.5])
+    else:
+        x_limit = np.array([x_center-(y_length/2)-0.5, x_center+(y_length/2)+0.5])
+        y_limit = np.array([y_center-(y_length/2)-0.5, y_center+(y_length/2)+0.5])
+
+    x_bins = np.arange(float(x_limit[0]), (float(x_limit[1]) + bin_size), bin_size)
+    y_bins = np.arange(float(y_limit[0]), (float(y_limit[1]) + bin_size), bin_size)
+
+    return x_bins, y_bins
+
 def visualize_configuration(top_file, dcd_files, config_file, bin_size=0.025, 
                             min_bin=0, max_frame=25, cartoon=False):
 
@@ -122,69 +148,110 @@ def visualize_configuration(top_file, dcd_files, config_file, bin_size=0.025,
     prm = get_comments_from_txt(config_file)
 
     # Read configuration txt file
-    try:
-        frames, x, y = np.loadtxt(config_file, unpack=True)
-    except:
-        x, y = np.loadtxt(config_file, unpack=True)
-        frames = np.arange(0, x.shape[0])
+    data = np.loadtxt(config_file)
+
+    if data.shape[1] == 2:
+        coord = np.fliplr(data[:,0:])
+        energy = None
+    elif data.shape[1] == 3:
+        coord = np.fliplr(data[:,1:])
+        frames = data[:,0]
+        energy = None
+    elif data.shape[1] == 4:
+        coord = np.fliplr(data[:,1:3])
+        frames = data[:,0]
+        energy = data[:,3]
+    else:
+        print('Error: Cannot read coordinates file! (#Columns: %s)' % data.shape[1])
+        sys.exit(1)
 
     # Calculate edges
-    xedges = np.arange(np.min(x), np.max(x) + bin_size, bin_size)
-    yedges = np.arange(np.min(y), np.max(y) + bin_size, bin_size)
+    edges_x, edges_y = assignbins2D(coord, bin_size)
 
     # Compute an histogram, just to have the maximum bin
-    H, xedges, yedges = np.histogram2d(x, y, bins=(xedges, yedges))
+    H, edges_x, edges_y = np.histogram2d(coord[:,0], coord[:,1], bins=(edges_x, edges_y))
 
     # Initialize histogram array and frame array
-    tmp = np.zeros(shape=(xedges.shape[0], yedges.shape[0], 1), dtype=np.int32)
-    H_frame = np.zeros(shape=(xedges.shape[0], yedges.shape[0], np.int(np.max(H))+100), dtype=np.int32)
+    tmp = np.zeros(shape=(edges_x.shape[0], edges_y.shape[0], 1), dtype=np.int32)
+    H_frame = np.zeros(shape=(edges_x.shape[0], edges_y.shape[0], np.int(np.max(H))+100), dtype=np.int32)
+
+    if energy is not None:
+        H_energy = np.empty(shape=(edges_x.shape[0], edges_y.shape[0], np.int(np.max(H))+100))
+        H_energy.fill(np.nan)
 
     # For each coordinate, we put them in the right bin and add the frame number
     for i in xrange(0, frames.shape[0]):
-        ix = np.int((x[i] - xedges[0]) / bin_size)
-        iy = np.int((y[i] - yedges[0]) / bin_size)
+        ix = np.int((coord[i,0] - edges_x[0]) / bin_size)
+        iy = np.int((coord[i,1] - edges_y[0]) / bin_size)
         
         H_frame[ix, iy, tmp[ix, iy]] = frames[i]
+        if energy is not None:
+            H_energy[ix, iy, tmp[ix, iy]] = energy[i]
         # Add 1 to the corresponding bin
         tmp[ix, iy] += 1
+
+    if energy is not None:
+        # get mean energy per bin
+        H_energy = np.nanmean(H_energy, axis=2)
     
     xx, yy = [], []
     id_to_H_frame = []
-    count, alpha = [], []
+    count, color, e = [], [], []
 
-    min_hist = np.float(np.min(H))
-    max_hist = np.float(np.max(H))
+    if energy is None:
+        min_hist = np.float(np.min(H))
+        max_hist = np.float(np.max(H))
+    else:
+        min_hist = np.float(np.nanmin(H_energy))
+        max_hist = np.float(np.nanmax(H_energy))
+
+    print('Min: %s Max: %s' % (min_hist, max_hist))
 
     # Add we keep only the bin with structure
     for i in xrange(0, H.shape[0]):
         for j in xrange(0, H.shape[1]):
 
             if H[i,j] > min_bin:
-                xx.append(xedges[i])
-                yy.append(yedges[j])
+                xx.append(edges_x[i])
+                yy.append(edges_y[j])
                 id_to_H_frame.append((i,j))
                 count.append(H[i,j])
 
-                value = 1. - (np.float(H[i,j]) - min_hist) / (max_hist - min_hist)
-                alpha.append(generate_color(value, 'jet'))
+                if energy is None:
+                    value = 1. - (np.float(H[i,j]) - min_hist) / (max_hist - min_hist)
+                else:
+                    value = (np.float(H_energy[i,j]) - min_hist) / (max_hist - min_hist)
+                    e.append(H_energy[i,j])
+
+                color.append(generate_color(value, 'jet'))
 
     TOOLS = "wheel_zoom,box_zoom,undo,redo,box_select,save,resize,reset,hover,crosshair,tap,pan"
 
     if prm:
-        title = '#Conf: %s Cycles: %s Rc: %s (Stress: %5.4f Corr: %5.4f)' % (frames.shape[0], 
-                 prm['cycle'], prm['rc'], np.float(prm['stress']), np.float(prm['corr']))
+        title = ""
+        for key in prm.iterkeys():
+            title += "%s: %s " % (key, prm[key])
     else:
-        title = '#Conf: %s' % frames.shape[0]
+        title = '#conformations: %s' % frames.shape[0]
 
     p = figure(plot_width=850, plot_height=850, tools=TOOLS, title=title, 
                webgl=True, title_text_font_size='12pt')
 
     source = ColumnDataSource(data={'xx': xx, 'yy': yy, 'count': count})
-    p.rect('xx', 'yy', width=bin_size, height=bin_size, color=alpha,
-           line_alpha=alpha, line_color='black', source=source)
+    if energy is not None:
+        source.add(e, name='energy')
+
+    # Create histogram
+    p.rect('xx', 'yy', width=bin_size, height=bin_size, color=color,
+           line_alpha=color, line_color='black', source=source)
+
+    # Create Hovertools
+    tooltips = [("(X, Y)", "(@xx @yy)"), ("#Frames", '@count')]
+    if energy is not None:
+        tooltips += [("Energy (Kcal/mol)", "@energy")]
 
     hover = p.select({'type': HoverTool})
-    hover.tooltips = [("(X, Y)", "(@xx @yy)"), ("#Frames", '@count')]
+    hover.tooltips = tooltips
 
     output_server('visualize')
 
