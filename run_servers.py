c6429ce59601ea8001a99b77ff3a27e1e7c4bbc7
Jérôme Eberhardt <jeeberhardt@users.noreply.github.com>
2016-10-27 14:36:18 +0200

    Minor changes

--- run_servers.py
+++ run_servers.py
@@ -9,9 +9,8 @@ import time
 import shlex
 import subprocess
 
-__author__ = "Jérôme Eberhardt, Roland H Stote, and Annick Dejaegere"
+__author__ = "Jérôme Eberhardt"
 __copyright__ = "Copyright 2016, Jérôme Eberhardt"
-__credits__ = ["Jérôme Eberhardt", "Roland H Stote", "Annick Dejaegere"]
 
 __lience__ = "MIT"
 __maintainer__ = "Jérôme Eberhardt"
